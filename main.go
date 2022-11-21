a706daa9868d1bb509f015fe4df9c4d201c70d30
Nidhey Nitin Indurkar <nidhey60@gmail.com>
2022-11-21 12:25:26 +0000

    server created

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	initializers "github.com/nidhey27/go-jwt/initilizers"
+)
+
+func init() {
+	initializers.LoadEnvVariables()
+	initializers.ConnectToDB()
+}
 
 func main() {
-	fmt.Println("")
+
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"status":  true,
+			"message": "GO JWT Authentication - POSTGRES API",
+			"error":   "",
+			"author":  "Nidhey Indurkar",
+			"data":    make([]string, 0),
+		})
+	})
+
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
